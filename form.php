b6847fdfcbce271cb0f8358dcd2b89e7c5977ab7
iqbalesta <111202214065@mhs.dinus.ac.id>
2022-10-10 00:16:42 +0700

    MINGGU-4

---
+++ form.php
@@ -0,0 +1,61 @@
+<?php
+$nisn=$_POST['nisn'];
+$asal_sekolah=$_POST['asal_sekolah'];
+$tahun_lulus_1=$_POST['tahun_lulus_1'];
+$tahun_lulus_2=$_POST['tahun_lulus_2'];
+$nama_lengkap=$_POST['nama_lengkap'];
+$nik=$_POST['nik'];
+$tempat=$_POST['tempat'];
+$tanggal_lahir=$_POST['tanggal_lahir'];
+$akta=$_POST['akta'];
+$jk=$_POST['jk'];
+$alamat=$_POST['alamat'];
+$alamat_2=$_POST['alamat_2'];
+$desa=$_POST['desa'];
+$desa_2=$_POST['desa_2'];
+$kecamatan=$_POST['kecamatan'];
+$kabupaten=$_POST['kabupaten'];
+$provinsi=$_POST['provinsi'];
+$kecamatan_2=$_POST['kecamatan_2'];
+$kabupaten_2=$_POST['kabupaten_2'];
+$provinsi_2=$_POST['provinsi_2'];
+
+$email=$_POST['email'];
+$nomor_kartu_keluarga=$_POST['nomor_kartu_keluarga'];
+$kartu_keluarga=$_POST['kartu_keluarga'];
+$nama_ayah_kandung=$_POST['nama_ayah_kandung'];
+$agama_1=$_POST['agama_1'];
+$pekerjaan_1=$_POST['pekerjaan_1'];
+$nama_ibu_kandung=$_POST['nama_ibu_kandung'];
+$agama_2=$_POST['agama_2'];
+$pekerjaan_2=$_POST['pekerjaan_2'];
+
+echo "NISN : ".$nisn."<br>";
+echo "Asal Sekolah : ".$asal_sekolah."<br>";
+echo "Tahun Lulus : ".$tahun_lulus_1."<br>";
+echo "Nama Lengkap : ".$nama_lengkap."<br>";
+echo "NIK : ".$nik."<br>";
+echo "Tempat : ".$tempat."Tanggal Lahir : ".$tanggal_lahir."<br>";
+echo "Akta Kelahiran yang Telah Dilegalisir : ".$akta."<br>";
+echo "Jenis Kelamin : ".$jk."<br>";
+echo "Alamat : ".$alamat."<br>";
+echo "Desa/Kel. : ".$desa."<br>";
+echo "Kecamatan: ".$kecamatan."<br>";
+echo "Kabupaten : ".$kabupaten."<br>";
+echo "Provinsi : ".$provinsi."<br>";
+
+echo "Email Aktif : ".$email."<br>";
+echo "Nomor Kartu Keluarga : ".$nomor_kartu_keluarga."<br>";
+echo "Nama Ayah Kandung : ".$nama_ayah_kandung."<br>";
+echo "Agama : ".$agama_1."<br>";
+echo "Pekerjaan : ".$pekerjaan_1."<br>";
+echo "Nama Ibu Kandung : ".$nama_ibu_kandung."<br>";
+echo "Agama : ".$agama_2."<br>";
+echo "Pekerjaan : ".$pekerjaan_2."<br>";
+
+echo "Alamat : ".$alamat_2."<br>";
+echo "Desa/Kel. : ".$desa_2."<br>";
+echo "Kecamatan: ".$kecamatan_2."<br>";
+echo "Kabupaten : ".$kabupaten_2."<br>";
+echo "Provinsi : ".$provinsi_2."<br>";
+?>
